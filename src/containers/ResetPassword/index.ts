835dde1055803bb1740cc88fd80a74abbda450b2
cyo23 <lsd000906@gmail.com>
2024-05-11 18:48:46 +1000

    merge all the commit together

--- src/containers/ResetPassword/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as ResetPasswordInitiationPage } from './ResetPasswordInitiationPage';
-export { default as PasswordResetLinkSentPage } from './components/PasswordResetSuccess';
-export { default as ResetPasswordForm } from './components/ResetPasswordForm';
--- src/shared/utils/helpers.ts
+++ src/shared/utils/helpers.ts
@@ -2,3 +2,4 @@ export const emailPatter =
   /^[\w-]+(\.[\w-]+)*@([a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*?\.[a-zA-Z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/;
 export const urlPattern =
   /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)/;
+export const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,32}$/;
